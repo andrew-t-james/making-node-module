91e5a9177a2a54ecadf6a178930694c4e34e10f1
Andrew James <ajames20@gmail.com>
2018-05-29 10:15:42 -0600

    Add bubbleSort

---
+++ index.js
@@ -0,0 +1,18 @@
+const bubbleSort = arr => {
+  let temp;
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+};
+
+module.exports = {
+  bubbleSort,
+};
